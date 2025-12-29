b8afd529dfdf71febec595f0fc79725b16c97774
Fau-Caudullo <128932666+Fau-Caudullo@users.noreply.github.com>
2025-12-29 18:38:51 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Alza il limite dell'avviso a 1000kb
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        // Separa le librerie esterne (come Supabase o React) in file diversi
+        // per velocizzare il caricamento iniziale
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
